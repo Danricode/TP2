193ba5b42d408ff77b5034fbf984b34924102340
Daniel Silva <danielaudrasilva@gmail.com>
2022-05-13 14:41:38 +0200

    Reùake of the previous TP using components

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, FlatList, Image, StyleSheet } from "react-native";
+import tp2 from "./tp2.json";
 
-export default function App() {
+const canvas = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <FlatList
+        data={tp2}
+        showsVerticalScrollIndicator={true}
+        renderItem={({ item }) => (
+          <View style={styles.container}>
+            <Text style={styles.title}>{item.title}</Text>
+            <View style={styles.titleDescription}>
+              <Image style={styles.image} source={{ uri: item.img }} />
+              <Text style={styles.description}>{item.desc}</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
+export default canvas;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 0,
+    padding: 30,
+    backgroundColor: "#FFD133",
   },
+  title: {
+    color: "black",
+    fontSize: 40,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    marginBottom: 10,
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  description: {
+    marginTop: 10,
+    fontSize: 18,
+  }
 });
